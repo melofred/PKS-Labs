372c20331015d05c7c66f60372e8d9e0e6fbc9ba
Fred Melo <fmelo@Freds-MacBook-Pro.local>
2018-08-16 16:59:37 -0700

    improvements

---
+++ 5-Helm/helm.sh
@@ -0,0 +1,7 @@
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
+# helm install --name my-release --set persistence.storageClass=default stable/mysql
+# helm delete my-release
+# troubleshoot: kubectl get events
