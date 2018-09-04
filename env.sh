5d5360df62015e635264b8a516329d28f1454c22
Fred Melo <fmelo@Freds-MacBook-Pro.local>
2018-09-04 14:47:34 -0700

    reviewing

--- env.sh
+++
@@ -1,8 +0,0 @@
-# connect to VPN first
-
-pks login -k -a api.pks.haas-63.pez.pivotal.io -u fmelo -p fmelo
-pks clusters
-
-# show kubectl config. If poiting to the wrong cluster, one needs to use "pks get-credentials <cluster-name>" in order to config kubectl
-
-kubectl cluster-info 
